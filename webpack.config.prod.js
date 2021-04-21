abf366b4cec31feada817dc042f0cedbe3132c47
pixel-fabian <82926714+pixel-fabian@users.noreply.github.com>
2021-04-21 11:33:59 +0200

    :construction: ADD basic files

---
+++ webpack.config.prod.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: './src/js/script.ts',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist/js'),
+    filename: 'script.js',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+      },
+    ],
+  },
+};
